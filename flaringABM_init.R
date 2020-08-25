6cd19bb73836c19c8d05e0858e1830e75f921a61
Nick Willems <nwillems@utexas.edu>
2020-08-25 13:18:33 -0500

    linked firms to a wells dataframe so firms can have more than 1 well

--- flaringABM_init.R
+++ flaringABM_init.R
@@ -1,7 +1,45 @@
-# Assign agent attributes
+### ASSIGN WELL ATTRIBUTES ###
+
+wells <- data.frame("wellID"=1:400)
+
+## RESOURCE
+wells[,"oil_BBL"] <- sample.int(40, size=nrow(wells), replace=TRUE) + 10
+wells[,"gas_MCF"] <- sample.int(40, size=nrow(wells), replace=TRUE) + 10
+
+## COSTS
+
+# standard operating cost without mitigation
+wells[,"baseline_oCost"] <- pmax(rnorm(nrow(wells), mean=50, sd=2), 10)
+
+# addtional fixed cost of mitigation
+wells[,"green_fCost"] <- pmax(rnorm(nrow(wells), mean=500, sd=100), 100)
+
+# addtional operating cost with mitigation
+wells[,"green_add_oCost"] <- pmax(rnorm(nrow(wells), mean=15, sd=5), 1)
+
+
+
+
+### ASSIGN AGENT ATTRIBUTES ###
 
 # initialize firms, none of whom are under social pressure or mitigating
-firms <- data.frame("id"=1:100, "mitigation"=0, "sPressure"=0, "cash"=0, "market_value"=NA)
+firms <- data.frame("firmID"=1:100, "mitigation"=0, "sPressure"=0, "cash"=0, "market_value"=NA)
+
+# randomly assign firms to wells
+well2firm <- sample(nrow(firms), nrow(wells), replace=TRUE)
+firms[,"wellID"] <- sapply(firms[,"firmID"], function(x)
+                            paste(
+                                which(well2firm==x),
+                                collapse = ";"))
+# redistribute so every firm has at least 1 well
+while (min(sapply(firms["wellID"], nchar))==0) {
+    max_wells <- which.max(sapply(firms["wellID"], function(x) lengths(gregexpr(";", x))))
+    firms[which.min(sapply(firms["wellID"], nchar)), "wellID"] <- sub(";.*", "", firms[max_wells, "wellID"])
+    firms[max_wells, "wellID"] <- sub(".*?;", "", firms[max_wells, "wellID"])
+}
+
+length(strsplit(paste(firms[,"wellID"], collapse = ";"), ";")[[1]])
+length(unique(strsplit(paste(firms[,"wellID"], collapse = ";"), ";")[[1]]))
 
 # Attributes from Hartley 2013: zotero://select/items/1_IKQGEEBK
 # oil & gas reserves as a proxy for     upstream capital 
@@ -11,18 +49,24 @@ firms[,"gas_reserves"] <- 1
 # refining capacity as a proxy for      downstream capital
 firms[,"ref_capacity"] <- sample.int(40, size=nrow(firms), replace=TRUE) + 10
 
+firms[,"gas_output"] <- sapply(strsplit(firms[,"wellID"], ";"), function(x) sum(subset(wells, wellID %in% x, gas_MCF)))
+if (sum(firms[,"gas_output"]) != sum(wells[,"gas_MCF"])) {
+    warning("well gas ouputs do not match firms")
+}
 
-firms[,"gas_output"] <- sample.int(40, size=nrow(firms), replace=TRUE) + 10
 
 ## COSTS
 # standard operating cost without mitigation
-firms[,"baseline_oCost"] <- pmax(rnorm(nrow(firms), mean=50, sd=2), 10)
+firms[,"baseline_oCost"] <-
+    sapply(strsplit(firms[,"wellID"], ";"), function(x) sum(subset(wells, wellID %in% x, baseline_oCost)))
 
 # addtional fixed cost of mitigation
-firms[,"green_fCost"] <- pmax(rnorm(nrow(firms), mean=500, sd=100), 100)
+firms[,"green_fCost"] <-
+    sapply(strsplit(firms[,"wellID"], ";"), function(x) sum(subset(wells, wellID %in% x, green_fCost)))
 
 # addtional operating cost with mitigation
-firms[,"green_add_oCost"] <- pmax(rnorm(nrow(firms), mean=15, sd=5), 1)
+firms[,"green_add_oCost"] <-
+    sapply(strsplit(firms[,"wellID"], ";"), function(x) sum(subset(wells, wellID %in% x, green_add_oCost)))
 
 firms[,"oCost"] <- firms[,"baseline_oCost"]
 
@@ -31,3 +75,4 @@ firms[,"t_horizon"] <- 5 #time horizon for decision making
 firms[,"i_horizon"] <- 4 #time horizon over which investments are paid off
 
 firms[,"t_switch"] <- NA #time at which agent switches into the green market
+
