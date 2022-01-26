6b9299cd75d83b06706605af0fe29ddbd86bc3f9
Nick Willems <nwillems@utexas.edu>
2022-01-26 08:38:47 -0600

    reconfigured postproc for memory constraints

--- flaringABM_postproc.R
+++ flaringABM_postproc.R
@@ -4,6 +4,27 @@ library(data.table)
 library(DBI)
 require(RSQLite)
 
+write_outputs <- function(db, csv, ID, append_file) {
+    print(paste("Compiling model:", names(ID)))
+
+    lease_states <- fread(sprintf("./outputs/lease_states_%s.csv", ID))
+    agent_states <- fread(sprintf("./outputs/agent_states_%s.csv", ID))
+
+    lease_states[, "model":= names(ID)]
+    agent_states[, "model":= names(ID)]
+
+    agent_states[
+        lease_states[status=="producing" & class=="underdeveloped", sum(csgd_MCF), by=.(model, RunID, time, firmID)],
+            on=c("model", "RunID", "time", "firmID"), "gas_flared":= V1]
+    agent_states[!is.na(time) & is.na(gas_flared), "gas_flared":= 0]
+
+    # write outputs
+    fwrite(agent_states, sprintf("./outputs/processed/agent_states_%s.csv.gz", csv), append=append_file)
+    dbWriteTable(all_states, "agent_states", agent_states, append=append_file)
+    dbWriteTable(all_states, "lease_states", lease_states, append=append_file)
+}
+
+
 jobIDs <- lapply(strsplit(args, "="), `[[`, 2)
 names(jobIDs) <- gsub("\\n", "\n", sapply(strsplit(args, "="), `[[`, 1), fixed=TRUE)
 print(jobIDs)
@@ -12,29 +33,7 @@ print(jobIDs)
 all_states <- dbConnect(RSQLite::SQLite(),
                 sprintf("./outputs/processed/all_states_%s.sqlite", paste(jobIDs, collapse="-")))
 
-
-print("Compiling lease states")
-lease_states <- rbindlist(lapply(jobIDs, function(x) fread(sprintf("./outputs/lease_states_%s.csv", x))), idcol="model")
-
-dbWriteTable(all_states, "lease_states", lease_states)
-
-
-print("Compiling agent states")
-agent_states <- rbindlist(lapply(jobIDs, function(x) fread(sprintf("./outputs/agent_states_%s.csv", x))), idcol="model")
-
-# calculate gas flared by each firm per time step
-agent_states[
-    lease_states[status=="producing" & class=="underdeveloped", sum(csgd_MCF), by=.(model, RunID, time, firmID)],
-        on=c("model", "RunID", "time", "firmID"), "gas_flared":= V1]
-agent_states[!is.na(time) & is.na(gas_flared), "gas_flared":= 0]
-
-rm(lease_states)
-
-fwrite(agent_states, sprintf("./outputs/processed/agent_states_%s.csv.gz", paste(jobIDs, collapse="-")))
-dbWriteTable(all_states, "agent_states", agent_states)
-
-rm(agent_states)
-
+lapply(seq(jobIDs), function(i) write_outputs(all_states, paste(jobIDs, collapse="-"), jobIDs[i], i!=1))
 
 dbDisconnect(all_states)
 
